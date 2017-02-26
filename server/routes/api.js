d6a491f49898f631dc60b86a2bed3c2619606a50
Raghu <Raghu>
2017-02-26 08:54:37 +0530

    changes for mongodb

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,26 +1,28 @@
 const express = require('express');
 const router = express.Router();
 
+
+
 //const axios = require('axios');
 
-//var Place = require('./server/models/place');
+var Place = require('./server/models/place');
 
 /* GET api listing. */
 router.get('/', (req, res) => {
   res.send('api works');
 });
 
-var MongoClient = require('mongodb').MongoClient
+//var MongoClient = require('mongodb').MongoClient
 
-MongoClient.connect('mongodb://mraghuram:zxcvZXCV@9@ds161069.mlab.com:61069/dump', function (err, db) {
-  if (err) throw err
+// MongoClient.connect('mongodb://mraghuram:zxcvZXCV@9@ds161069.mlab.com:61069/dump', function (err, db) {
+//   if (err) throw err
 
-  db.collection('nearyby').find().toArray(function (err, result) {
-    if (err) throw err
+//   db.collection('nearyby').find().toArray(function (err, result) {
+//     if (err) throw err
 
-    console.log(result)
-  })
-})
+//     console.log(result)
+//   })
+// })
 
 //router.get('/list', (req, res) => {
   // Get posts from the mock api
