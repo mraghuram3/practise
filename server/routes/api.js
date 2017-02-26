4b5893f0b1911f420e35fb05fd092167be45738e
Raghu <Raghu>
2017-02-26 13:58:32 +0530

    adding service

--- server/routes/api.js
+++ server/routes/api.js
@@ -22,6 +22,7 @@ router.get('/list', (req, res) => {
     db.close();
      var obj=({data: result});
      var body = JSON.stringify(obj);
+     res.header("Content-Type", "application/json");
     res.send(body);
   })
 })
@@ -41,6 +42,7 @@ router.get('/count', (req, res) => {
         console.log(result);
         var obj=({data: result});
         var body = JSON.stringify(obj);
+        res.header("Content-Type", "application/json");
          res.send(body);
 	  }
   );
